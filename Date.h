cdc01555ae36975e59496469f4b185a99eb1a02b
Starinin Andrey <starininandrey@gmail.com>
2023-04-29 11:24:42 +0300

    Update

--- Date.h
+++ Date.h
@@ -1,10 +1,43 @@
 #ifndef STRUCTDEMO_DATE_H
 #define STRUCTDEMO_DATE_H
 
+#include <string>
+
+using namespace std;
+
+enum DateFormat {
+    Normal = 1, // dd.mm.yyyy
+    ReverseNormal = 2, // yyyy.mm.dd
+    SpecialNormal = 3, // dd-mm-yyyy
+    ReverseSpecialNormal = 4 // yyyy-mm-dd
+};
+
 struct Date {
     int day;
     int month;
     int year;
+
+    string ToString(DateFormat format = DateFormat::Normal) {
+        switch (format) {
+            case DateFormat::Normal: // dd.mm.yyyy
+                return to_string(day) + "." + to_string(month) + "." + to_string(year);
+            case DateFormat::ReverseNormal: // yyyy.mm.dd
+                return to_string(year) + "." + to_string(month) + "." + to_string(day);
+            case DateFormat::SpecialNormal:
+                return to_string(day) + "-" + to_string(month) + "-" + to_string(year);
+            case DateFormat::ReverseSpecialNormal: // yyyy-mm-dd
+                return to_string(year) + "-" + to_string(month) + "-" + to_string(day);
+        }
+
+    }
 };
+
+Date CreateDate(int year, int month, int day) {
+    Date date;
+    date.year = year;
+    date.month = month;
+    date.day = day;
+    return date;
+}
 
 #endif //STRUCTDEMO_DATE_H
--- main.cpp
+++ main.cpp
@@ -14,10 +14,7 @@ int main() {
     person.date_of_birth.month = 2;
     person.date_of_birth.year = 1986;
 
-    Date date;
-    date.day = 1;
-    date.month = 1;
-    date.year = 1;
+    Date date = CreateDate(1, 1, 1);
     Person person2 = CreatePerson("Dora", "Starinina", date);
 
     PrintPerson(person);
@@ -27,5 +24,5 @@ int main() {
 }
 
 void PrintPerson(Person person) {
-    cout << person.last_name << " " << person.first_name << ", " << person.date_of_birth.day << "." << person.date_of_birth.month << "." << person.date_of_birth.year << endl;
+    cout << person.last_name << " " << person.first_name << ", " << person.date_of_birth.ToString(DateFormat::ReverseSpecialNormal) << endl;
 }
